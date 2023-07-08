25e9e6759e1958e6a1ff4db8c12e53891d19460f
Livni <llmc0383@gmail.com>
2023-07-08 01:23:44 -0600

    se modifico formulario y se realiza prueba con documento js

--- index.php
+++ index.php
@@ -1,119 +1,33 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styleloginemb.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <title>LIVZ'YATES MARTZ'VIP</title>
+  <title>TAREA 2</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
- <!-- Navegación -->
- <nav class="navbar navbar-expand-lg navbar-light muted">
-    <img src= "imagenes/Logo_livz.png" width="200px" height="110px">
-    <div class="container">
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">Nuestros Yates</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">Quienes Somos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">Fichas Tecnicas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="">Tienda Online</a>
-        </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">Contactanos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">Redes Sociales</a>
-            </li>
-        </ul>
+<div class="container mt-3">
+  <h2>FORMULARIO</h2>
+  <form id="formulario"> 
+  <div class="mb-3 mt-3">
+      <label for="id">Identificador:</label>
+      <input type="number" class="form-control" id="id" placeholder="Ingrese su codigo identificador" name="id">
+  </div>
+  <div class="mb-3 mt-3">
+      <label for="name">Nombres:</label>
+      <input type="text" class="form-control" id="nombre" placeholder="Ingrese sus Nombres" name="name">
+  </div>
+    <div class="mb-3 mt-3">
+      <label for="clases">Clases:</label>
+      <input type="text" class="form-control" id="clases" placeholder="Ingrese clases" name="clases">
     </div>
-    </div>
-</nav>
-<br>
-    <!----- Seccion titular ------------->    
-  <h1><b>Descubre la emoción de navegar en un espectacular yate de lujo con nuestra selección exclusiva en YATES MARTZ'VIP.</b></h1>
-
-
-   <!-- Contenedor formulario -->
-   <section class="text-center"></section>
-   <div class="container py-3">
-     <div class="row g-0 align-items-center">
-       <div class="col-lg-6 mb-5 mb-lg-0">
-         <div class="card cascading-right" style="background: hsla(188, 61%, 64%, 0.462);
-             backdrop-filter: blur(20px);">
-           <div class="card-body p-5 shadow-5 text-center">
-             <h2 class="fw-bold mb-5">Registrate!!!</h2>
-             <form>
-               <!-- El formulario para registrarse -->
-               <div class="row">
-                  <!-- nombre input -->
-                 <div class="col-md-6 mb-4">
-                   <div class="form-outline">
-                     <input type="text" id="name" class="form-control" required />
-                     <label class="form-label" for="Name" >Nombres</label>
-                   </div>
-                 </div>
-                   <!-- apellido input -->
-                 <div class="col-md-6 mb-4">
-                   <div class="form-outline">
-                     <input type="text" id="Apellido" class="form-control" required />
-                     <label class="form-label" for="Apellidos">Apellidos</label>
-                   </div>
-                 </div>
-               </div>
- 
-               <!-- correo input -->
-               <div class="form-outline mb-4">
-                 <input type="email" id="email" class="form-control" required/>
-                 <label class="form-label" for="email">Correo Electronico</label>
-               </div>
- 
-               <!-- Contraseña input -->
-               <div class="form-outline mb-4">
-                 <input type="password" id="password" class="form-control" required />
-                 <label class="form-label" for="password">Contraseña</label>
-               </div>
- 
-               <!-- Checkbox para suscribirse --------->
-               <div class="form-check d-flex justify-content-center mb-4">
-                 <input class="form-check-input me-2" type="checkbox" value="" id="checkbox" checked />
-                 <label class="form-check-label" for="checkbox">¿Deseas guardar tus datos en el ordenador?</label>
-               </div>
- 
-               <!-- Redireccion al Inicio de la pagina web -->
-               <a href="homeemb.html" class="btn btn-primary btn-block mb-4" >Ingrese aquí </a>
-             </form>
-           </div>
-         </div>
-       </div>
-        <!-- imagen del login -->
-       <div class="col-lg-6 mb-5 mb-lg-0">
-         <img src="imagenes/yateinicio.jpg" id= "img" class="w-100 rounded-4 shadow-4"
-           alt="" />
-       </div>
-     </div>
-   </div>
- </section>
- <!-- Section: Design Block -->
-      <section>
-        <div class="text-center py-4 px-4 bg-black-50">
-          <!-- Copyright -->
-          <div class="text-center text-white mb-3"> 
-            Copyright © 2023. Todos los derechos son reservados, creado por Livni Martinez.
-          </div>
-      </section>
+   
+    <button type="submit" class="btn btn-primary">Ingresar</button>
+  </form>
+</div>
+<script src="script.js" ></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,4 @@
+// let form = document.getElementById(formulario)
+// console.log(form)
+let id = document.getElementById('#id')
+console.log(id)
