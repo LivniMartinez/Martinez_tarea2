f44fad415484e0825e953e3e6a0abd83845fec32
Livni <llmc0383@gmail.com>
2023-07-09 22:26:38 -0600

    Se agrego un href para utilizar los selectores

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // OBTENER ELEMENTOS DEL DOM
 // MEDIANTE SU ID
 let titulo = document.getElementById('tituloPagina');
-let boton = document.getElementById('botonClick')
+let boton = document.getElementById('botonClick');
 
 
 // let form = document.getElementById(formulario)
 // console.log(form)
-let id = document.getElementById('id')
-console.log(id)
+let id = document.getElementById('id');
+console.log(id);
+
+
+//Con Selectores
