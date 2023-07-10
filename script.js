2bafb6376452a57229124ed0c1ff987a01be1876
Livni <llmc0383@gmail.com>
2023-07-09 22:13:32 -0600

    elementos de DOM

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+// OBTENER ELEMENTOS DEL DOM
+// MEDIANTE SU ID
+let titulo = document.getElementById('tituloPagina');
+let boton = document.getElementById('botonClick')
+
+
 // let form = document.getElementById(formulario)
 // console.log(form)
-let id = document.getElementById('#id')
+let id = document.getElementById('id')
 console.log(id)
